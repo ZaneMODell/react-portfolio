7d9daa1438b047d2c952ee3994be051b21258a4a
Zane ODell <odellzan@msu.edu>
2023-01-08 00:20:01 -0500

    More shit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {BsAward} from 'react-icons/bs'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know Me</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+        <div className="about__me-image">
+          <img src={ME} alt="" />
+        </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <BsAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3 years of college and 3 month software development internship</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Employers</h5>
+              <small>Michigan State University, Auto-Owners Insurance</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>Multiple video game projects, React development projects, wxWidgets software development</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {AiOutlineCode} from 'react-icons/ai'
+import {AiOutlineBook} from 'react-icons/ai'
+import {MdContactMail} from 'react-icons/md'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><AiOutlineCode /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineBook /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdContactMail /></a>
+    </nav>
   )
 }
 
